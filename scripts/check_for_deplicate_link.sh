8b054228fda9c46b71c523b314169b57c893268f
kondo takeshi <chaspy@users.noreply.github.com>
2021-02-09 01:21:12 +0900

    Add scripts for checking deplicate link

---
+++ scripts/check_for_deplicate_link.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Script for checking duplicate link.
+# First, extract the link line that starts with "^- ".
+# Next, read the next line one by one and extract the url with regex.
+# Lastly, if a duplicate url exists, return an error  3 # Script for checking duplicate link.
+LINKPATH="content/links.md"
+REGEX='(http(s?))://[A-Za-z0-9\+&@#/%?=~_|!:,.;]+'
+
+grep -E "^- " "${LINKPATH}" | while read line
+do
+  if [[ $line =~ $REGEX ]];then 
+    url="${BASH_REMATCH[0]}"
+    count=$(grep -c "$url" "${LINKPATH}")
+    if [ $count -gt 1 ]; then
+      echo "found duplicate link. ${url}"
+      exit 1
+    fi
+  fi
+done
